84d45b0dd3e4341bb01096e925393777521ac730
Kieran Alden <kieran.alden@york.ac.uk>
2017-10-26 14:19:04 +0100

    Fixed error in generate_model_formula test

--- tests/testthat/test_emulation_utilities.R
+++ tests/testthat/test_emulation_utilities.R
@@ -3,6 +3,6 @@ context("Testing Spartan Utilities Class")
 
 test_that("generate_model_formula", {
 
-  expect_equal(generate_model_formula(c("ChemokineThreshold","AdhesionResponse"),c("Velocity","Dislacement")),as.formula("Velocity + Displacement ~ ChemokineThreshold + AdhesionResponse",env=parent.frame()))
+  expect_equal(generate_model_formula(c("ChemokineThreshold","AdhesionResponse"),c("Velocity","Displacement")),as.formula("Velocity + Displacement ~ ChemokineThreshold + AdhesionResponse",env=parent.frame()))
 
 })
